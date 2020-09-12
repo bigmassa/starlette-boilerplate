davidaccent/boilerplate

added fast api and jwt auth

src/app/api/main.py
@@ -1,0 +1,3 @@
+from fastapi import FastAPI
+
+app = FastAPI(openapi_prefix="/api")

bigmassa/starlette-boilerplate

src/app/api/main.py
@@ -1,0 +1,3 @@
+from fastapi import FastAPI
+
+app = FastAPI(root_path="/api")